f13f8cc1b567c7c7ab2204b1acb48dd3753e493e
cleverson mendes <kesojotta@gmail.com>
2022-03-23 19:57:20 -0300

    subindo

---
+++ hello/src/main/java/com/generation/helloworld/controller/helloWorldController.java
@@ -0,0 +1,43 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello-world")
+public class helloWorldController {
+
+	@GetMapping
+	public String helloworld() {
+  return "hello world , o pai ta on  ";
+	}
+	
+	@GetMapping("/hello world")
+	public String turma47() {
+  return "hello world ";
+	}
+	@GetMapping("/BSM")
+	public String listaBSM() {
+		
+  return "Mentalidade de Crescimento\r\n"
+  		+ "Persistência\r\n"
+  		+ "Responsabilidade Pessoal\r\n"
+  		+ "Orientação ao Futuro\r\n"
+  		+ "Comunicação\r\n"
+  		+ "Orientação ao Detalhe\r\n"
+  		+ "Proatividade\r\n"
+  		+ "Trabalho em Equipe";
+  
+	}@GetMapping("/Objetivos")
+	public String MeusObjetivo() {
+		
+	  return "Mentalidade de Crescimento\r\n"
+	  		+ "Persistência aprendendo SQL\r\n"
+	  		+ "Quero aprender a colocar meu site no ar com spring\r\n"
+	  		+ "Deixar meu site funcional com a conecção com o banco dedos"
+	  	    ;
+	  	
+	  
+}
+}
